2555abe43f1d6a627832f1cd27c59a4a378c6b71
Nehaps29 <133929172+Nehaps29@users.noreply.github.com>
2023-07-29 15:50:33 -0400

    added color to each time block as past, present, future as per current time

--- script.js
+++ script.js
@@ -78,6 +78,150 @@ function saveProjectsToStorage(projects) {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  
+  const d = new Date();
+  let hour = d.getHours();
+
+  if (hour == 9){
+    $("#hour-9").removeClass("past");
+    $("#hour-9").removeClass("future");
+    $("#hour-9").addClass("present");
+  } else if (hour>9) { 
+    $("#hour-9").removeClass("present");
+    $("#hour-9").removeClass("future");
+    $("#hour-9").addClass("past");
+   }
+   else {
+    $("#hour-9").removeClass("present");
+    $("#hour-9").removeClass("past");
+    $("#hour-9").addClass("future");
+   }
+
+  if (hour == 10){
+    $("#hour-10").removeClass("past");
+    $("#hour-10").removeClass("future");
+    $("#hour-10").addClass("present");
+  } else if (hour>10) { 
+    $("#hour-10").removeClass("present");
+    $("#hour-10").removeClass("future");
+    $("#hour-10").addClass("past");
+   }
+   else {
+    $("#hour-10").removeClass("present");
+    $("#hour-10").removeClass("past");
+    $("#hour-10").addClass("future");
+   }
+
+
+  if (hour == 11){
+    $("#hour-11").removeClass("past");
+    $("#hour-11").removeClass("future");
+    $("#hour-11").addClass("present");
+  } else if (hour>11) { 
+    $("#hour-11").removeClass("present");
+    $("#hour-11").removeClass("future");
+    $("#hour-11").addClass("past");
+   }
+   else {
+    $("#hour-11").removeClass("present");
+    $("#hour-11").removeClass("past");
+    $("#hour-11").addClass("future");
+   }
+
+  if (hour == 12){
+    $("#hour-12").removeClass("past");
+    $("#hour-12").removeClass("future");
+    $("#hour-12").addClass("present");
+  } else if (hour>12) { 
+    $("#hour-12").removeClass("present");
+    $("#hour-12").removeClass("future");
+    $("#hour-12").addClass("past");
+   }
+   else {
+    $("#hour-12").removeClass("present");
+    $("#hour-12").removeClass("past");
+    $("#hour-12").addClass("future");
+   }
+
+  if (hour == 13){
+    $("#hour-13").removeClass("past");
+    $("#hour-13").removeClass("future");
+    $("#hour-13").addClass("present");
+  } else if (hour>13) { 
+    $("#hour-13").removeClass("present");
+    $("#hour-13").removeClass("future");
+    $("#hour-13").addClass("past");
+   }
+   else {
+    $("#hour-13").removeClass("present");
+    $("#hour-13").removeClass("past");
+    $("#hour-13").addClass("future");
+   }
+
+  if (hour == 14){
+    $("#hour-14").removeClass("past");
+    $("#hour-14").removeClass("future");
+    $("#hour-14").addClass("present");
+  } else if (hour>14) { 
+    $("#hour-14").removeClass("present");
+    $("#hour-14").removeClass("future");
+    $("#hour-14").addClass("past");
+   }
+   else {
+    $("#hour-14").removeClass("present");
+    $("#hour-14").removeClass("past");
+    $("#hour-14").addClass("future");
+   }
+
+
+  if (hour == 15){
+    $("#hour-15").removeClass("past");
+    $("#hour-15").removeClass("future");
+    $("#hour-15").addClass("present");
+  } else if (hour>15) { 
+    $("#hour-15").removeClass("present");
+    $("#hour-15").removeClass("future");
+    $("#hour-15").addClass("past");
+   }
+   else {
+    $("#hour-15").removeClass("present");
+    $("#hour-15").removeClass("past");
+    $("#hour-15").addClass("future");
+   }
+
+
+  if (hour == 16){
+    $("#hour-16").removeClass("past");
+    $("#hour-16").removeClass("future");
+    $("#hour-16").addClass("present");
+  } else if (hour>16) { 
+    $("#hour-16").removeClass("present");
+    $("#hour-16").removeClass("future");
+    $("#hour-16").addClass("past");
+   }
+   else {
+    $("#hour-16").removeClass("present");
+    $("#hour-16").removeClass("past");
+    $("#hour-16").addClass("future");
+   }
+
+  
+  if (hour == 17){
+    $("#hour-17").removeClass("past");
+    $("#hour-17").removeClass("future");
+    $("#hour-17").addClass("present");
+  } else if (hour>17) { 
+    $("#hour-17").removeClass("present");
+    $("#hour-17").removeClass("future");
+    $("#hour-17").addClass("past");
+   }
+   else {
+    $("#hour-17").removeClass("present");
+    $("#hour-17").removeClass("past");
+    $("#hour-17").addClass("future");
+   }
+
+  
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -94,7 +238,7 @@ function saveProjectsToStorage(projects) {
 }*/
   // TODO: Add code to display the current date in the header of the page.
   var today = dayjs();
-  $ ('#currentDay').text(today.format('MMM D, YYYY'));
+  $ ('#currentDay').text(today.format('MMM D, YYYY, HH'));
 });
 
 
